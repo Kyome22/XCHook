2d21f1af03725f347f09e01eae8a490fc545e13d
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2022-05-22 23:45:43 +0900

    add timestamp to Message.swift

--- Sources/XCHook/swift_script/Message.swift
+++ Sources/XCHook/swift_script/Message.swift
@@ -7,14 +7,15 @@
 
 import Foundation
 
-guard CommandLine.arguments.count == 4 else {
+guard CommandLine.arguments.count == 5 else {
     NSLog("🔧 XCHook:Message.swift \(CommandLine.arguments.count)")
     exit(1)
 }
 let dict: [String: Any] = [
     "project": CommandLine.arguments[1],
     "path": CommandLine.arguments[2],
-    "status": CommandLine.arguments[3]
+    "status": CommandLine.arguments[3],
+    "timestamp": CommandLine.arguments[4]
 ]
 if let encodeData = try? JSONSerialization.data(withJSONObject: dict, options: []),
    let jsonStr = String(data: encodeData, encoding: .utf8) {
